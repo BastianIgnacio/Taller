e9dd079e2120cb9b23aa4045dea5930b4b062af2
Matias Escobar <mescobar13@alumnos.utalca.cl>
2019-10-07 16:37:07 -0300

    cambios pequeños en dashboard

--- tallerMecanico/resources/views/gestionarTecnicos/gestionarTecnicos.blade.php
+++ tallerMecanico/resources/views/gestionarTecnicos/gestionarTecnicos.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="row">
-        <div class="col-md-9 col-sm-9 col-xs-9">
+        <div class="col-md-12 col-sm-12 col-xs-12">
             <div class="x_panel">
                 <div class="x_title">
                     <h2>Listado de Tecnicos</h2>
