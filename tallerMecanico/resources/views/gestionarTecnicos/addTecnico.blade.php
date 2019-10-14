2cfea13626c86962a8e65e0eeb4586f2d1e5b390
Bastian <bastian.sepiulvedagarcia@gmail.com>
2019-10-14 20:21:52 -0300

    agregacion de required

--- tallerMecanico/resources/views/gestionarTecnicos/addTecnico.blade.php
+++ tallerMecanico/resources/views/gestionarTecnicos/addTecnico.blade.php
@@ -14,28 +14,28 @@
             <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method="POST" action="/gestionar-tecnicos/add-tecnico">
             @csrf
               <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nombre<span class="required"></span>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nombre *<span class="required"></span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
                   <input type="text" id="nombre" name="primernombre" required="required" class="form-control col-md-7 col-xs-12">
                 </div>
               </div>
               <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Apellido <span class="required"></span>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Apellido * <span class="required"></span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
                   <input type="text" id="apellido" name="primerapellido" required="required" class="form-control col-md-7 col-xs-12">
                 </div>
               </div>
               <div class="form-group">
-                <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">RUT</label>
+                <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">RUT *</label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
-                  <input id="rut"  name="rut" class="form-control col-md-7 col-xs-12" type="text" name="middle-name" data-inputmask="'mask': '99.999.999'">
+                  <input id="rut"  name="rut" class="form-control col-md-7 col-xs-12" type="text" name="middle-name" data-inputmask="'mask': '99.999.999'" required>
                 </div>
               </div>
 
               <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12">Correo<span class="required"></span>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12">Correo *<span class="required"></span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
                   <input type="email" id="correo" class="form-control" name="correo" data-parsley-trigger="change" required />
@@ -43,11 +43,11 @@
               </div>
 
               <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12">Numero<span class="required"></span>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12">Numero *<span class="required"></span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
       
-                  <input id="numero" name="numerocontacto" type="text" class="form-control" data-inputmask="'mask': '999-99999999'">
+                  <input id="numero" name="numerocontacto" type="text" class="form-control" data-inputmask="'mask': '999-99999999'" required >
                 </div>
               </div>
               <div class="ln_solid"></div>
