f52d919c1316e2400c8a7ca09ec87a936841d730
Bastian <bastian.sepiulvedagarcia@gmail.com>
2019-10-19 23:18:19 -0300

    edicion de algunos views

--- tallerMecanico/resources/views/inventario/addInventario.blade.php
+++ tallerMecanico/resources/views/inventario/addInventario.blade.php
@@ -17,21 +17,22 @@
             <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left">
 
               <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nombre<span class="required"></span>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Codigo<span class="required"></span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
                   <input type="text" id="nombre" required="required" class="form-control col-md-7 col-xs-12">
                 </div>
               </div>
               <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="last-name">Proveedor <span class="required"></span>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Nombre<span class="required"></span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
-                  <input type="text" id="proveedor" name="last-name" required="required" class="form-control col-md-7 col-xs-12">
+                  <input type="text" id="nombre" required="required" class="form-control col-md-7 col-xs-12">
                 </div>
               </div>
+              
               <div class="form-group">
-                <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Cantidad</label>
+                <label for="middle-name" class="control-label col-md-3 col-sm-3 col-xs-12">Marca</label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
                   <input id="cantidad" class="form-control col-md-7 col-xs-12" type="text" name="middle-name">
                 </div>
@@ -46,11 +47,12 @@
               </div>
 
               <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12">Fabricante<span class="required"></span>
+                <label class="control-label col-md-3 col-sm-3 col-xs-12">Cantidad Inicial<span class="required"></span>
                 </label>
                 <div class="col-md-6 col-sm-6 col-xs-12">
-                  <input id="fabricante" class="date-picker form-control col-md-7 col-xs-12" required="required" type="text">
+                    <input type="text" required="required" data-inputmask="'mask': '9999'" class="form-control col-md-7 col-xs-12">
                 </div>
+                
               </div>
 
               <div class="ln_solid"></div>
