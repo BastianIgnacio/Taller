d131439f129dc181843f05d58f3bc8331439040d
Bastian <bastian.sepiulvedagarcia@gmail.com>
2019-10-25 20:46:11 -0300

    Agregar Insumo funcionalidad Completa y testeada

--- tallerMecanico/app/Http/Controllers/InventarioController.php
+++ tallerMecanico/app/Http/Controllers/InventarioController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\QueryException;
+use App\Http\Controllers\Redirect;
 
 class InventarioController extends Controller
 {
@@ -18,41 +20,54 @@ class InventarioController extends Controller
     public function agregarInsumo(Request $request)
     {
 
+        //Creacion de mensajes de error y Validacion 
         $messages = [
             'cantidad.max'    => 'La cantidad es superior a 9999',
+            'cantidad.numeric' => 'La cantidad debe ser un numero, ej: 20 debe ser escrito 0020',
+            'descripcion.required' => ' La descripcion del insumo es obligatorio',
         ];
-        
-        //return $request;
+
         $validator = Validator::make($request->all(), 
         [
             'codigo' => 'required|max:50',
             'nombre' => 'required',
             'marca' => 'required',
             'modelo' => 'required',
-            'cantidad' => 'numeric|min:1|max:9999'
+            'cantidad' => 'numeric|min:1|max:9999',
+            'descripcion' => 'required'
         ], $messages
         );
 
         if ($validator->fails()) {
-            return $validator->errors();
+            return redirect('/gestionar-inventario/add-inventario')
+                        ->withErrors($validator)
+                        ->withInput();
         }
 
+        // Validado, ahora queda trabajar con los datos ya seguros.
 
         $codigo = $request->input("codigo");
         $nombre = $request->input("nombre");
         $marca = $request->input("marca");
         $modelo = $request->input("modelo");
         $cantidad = $request->input("cantidad");
-        
+        $descripcion = $request->input("descripcion");
+        //return $request;
 
-        //Necesitamos validar
+        try { 
+        //Your code
+            \DB::table('insumo')->insert(
+            ['codigo' => $codigo, 
+            'nombre' => $nombre,
+            'marca' => $marca ,
+            'modelo' => $modelo ,
+            'cantidad' =>  $cantidad,
+            'descripcion' => $descripcion,
+            ]);
 
-        
-
-
-
-        
-
+        } catch(QueryException $ex){ 
+            return redirect('/gestionar-inventario/add-inventario')->withErrors(['Problema al ingresar']);
+        }
     }
 
     public function agregarExistencias()
--- tallerMecanico/resources/views/inventario/addInventario.blade.php
+++ tallerMecanico/resources/views/inventario/addInventario.blade.php
@@ -1,5 +1,6 @@
 @extends('../layout.master')
 
+
 @section('content')
 
     <div class="x_panel">
@@ -7,6 +8,15 @@
             <h2>Agregar al Inventario</h2>
             <div class="clearfix"></div>
         </div>
+        @if ($errors->any())
+        <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+        </div>
+        @endif
         <div class="x_content">
             <br />
             <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" method="post" action="/agregarInsumo">
@@ -34,13 +44,21 @@
                 </div>
               </div>
 
-                <div class="form-group">
-                    <label class="control-label col-md-3 col-sm-3 col-xs-12">Modelo<span class="required"></span>
-                    </label>
-                    <div class="col-md-6 col-sm-6 col-xs-12">
-                        <input name="modelo" class="date-picker form-control col-md-7 col-xs-12" required="required" type="text">
-                    </div>
+              <div class="form-group">
+                  <label class="control-label col-md-3 col-sm-3 col-xs-12">Modelo<span class="required"></span>
+                  </label>
+                  <div class="col-md-6 col-sm-6 col-xs-12">
+                      <input name="modelo" class="date-picker form-control col-md-7 col-xs-12" required="required" type="text">
+                  </div>
+              </div>
+
+              <div class="form-group">
+                <label class="control-label col-md-3 col-sm-3 col-xs-12">Descripción<span class="required"></span>
+                </label>
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                  <textarea class="resizable_textarea form-control" required="required" name="descripcion" placeholder="Descripción del insumo"></textarea>
                 </div>
+              </div>
 
               <div class="form-group">
                 <label class="control-label col-md-3 col-sm-3 col-xs-12">Cantidad Inicial<span class="required"></span>
@@ -48,7 +66,6 @@
                 <div class="col-md-6 col-sm-6 col-xs-12">
                     <input type="text" name="cantidad" required="required" data-inputmask="'mask': '9999'" class="form-control col-md-7 col-xs-12">
                 </div>
-                
               </div>
 
                 <div class="ln_solid"></div>
